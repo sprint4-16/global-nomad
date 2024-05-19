221e89055cdec0ab81b90745426fe36767414f18
Youth-o <2021111495@dgu.ac.kr>
2024-05-19 14:37:27 +0900

    design: Input 컴포넌트 스타일 수정

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
-import eyeOff from '@/images/btn/btn_eye_off.svg';
-import eyeOn from '@/images/btn/btn_eye_on.svg';
+import EyeOff from '@/images/btn/btn_eye_off.svg';
+import EyeOn from '@/images/btn/btn_eye_on.svg';
 import styles from './Input.module.scss';
 import classNames from 'classnames/bind';
 
@@ -23,7 +23,7 @@ export function Input({ label, type, placeholder, color, className }: InputProps
       {type === 'password' ? (
         <>
           <input type={type} id={type} placeholder={placeholder} className={cx('input', color)} />
-          <img src={eyeOff} />
+          <EyeOff className={cx('img')} />
         </>
       ) : (
         <input type={type} id={type} placeholder={placeholder} className={cx('input', color)} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import { Input } from '@/components/Input/Input';
+
 export default function Home() {
-  return <>랜딩페이지</>;
+  return (
+    <>
+      랜딩페이지
+      <Input label={'이메일'} type={'email'} placeholder={'이메일을 작성해주세요'} />
+      <Input label={'비밀번호'} type={'password'} placeholder={'비밀번호를 작성해주세요'} />
+    </>
+  );
 }
