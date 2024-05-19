79a3f9339aebc5d3aa4ce8e296b2247358b0bb7b
KimChangmin <zzangmin3@gmail.com>
2024-05-19 14:17:43 +0900

    refactor: 페이지 이동 시 queryClient 재발생 문제 해결

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import '@/styles/reset.scss';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
