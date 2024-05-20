00263e96cdd392591279d58b67d16bf04f05028a
Taejin Kim <rlaxowls1316@naver.com>
2024-05-20 23:43:00 +0900

    feat: 버튼 컴포넌트 구현

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,24 @@
-import { ReactNode } from 'react';
+import { CSSProperties, ReactNode } from 'react';
 import styles from './Button.module.scss';
 import classNames from 'classnames/bind';
 
 interface ButtonProps {
   children: ReactNode;
-  type: ButtonType;
+  type: Type;
+  size: Size;
+  sx?: CSSProperties;
+  onClick?: () => void;
 }
 
-type ButtonType = 'primary' | 'secondary' | 'disabled';
+type Type = 'primary' | 'secondary' | 'disabled';
+type Size = 'large' | 'medium' | 'small' | 'full';
 
 const cn = classNames.bind(styles);
 
-export default function Button({ type, children }: ButtonProps) {
-  return <button className={cn(type)}>{children}</button>;
+export default function Button({ type, size, sx, onClick, children }: ButtonProps) {
+  return (
+    <button onClick={onClick} style={sx} className={cn('btn', type, size)}>
+      {children}
+    </button>
+  );
 }
