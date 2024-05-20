da64c6f95767cf8dd1410074d791af49dca0962c
KimChangmin <zzangmin3@gmail.com>
2024-05-20 14:16:55 +0900

    feat: 리액트 설정 완료

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import '@/styles/reset.scss';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
