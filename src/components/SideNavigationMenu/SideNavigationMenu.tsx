04ff31dfb1260a9ad13218c6742962326f958f83
김동현 <dtc03003@naver.com>
2024-05-21 13:36:58 +0900

    feat: 부모에 상태값 전달 구현

--- src/components/SideNavigationMenu/SideNavigationMenu.tsx
+++ src/components/SideNavigationMenu/SideNavigationMenu.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import classNames from 'classnames/bind';
 import styles from './SideNavigationMenu.module.scss';
 import ProfileImg from '@/images/img/img_resource_profile_img_sample.png';
@@ -6,33 +7,49 @@ import AccountCheck from '@/images/icon/icon_account_check.svg';
 import TextBoxCheck from '@/images/icon/icon_text_box_check.svg';
 import CalendarCheck from '@/images/icon/icon_calendar_check.svg';
 import Setting from '@/images/icon/icon_setting.svg';
-import Image from 'next/image';
 
-const navItems = [
-  { id: 1, icon: <AccountCheck />, title: '내 정보' },
-  { id: 2, icon: <TextBoxCheck />, title: '예약 내역' },
-  { id: 3, icon: <CalendarCheck />, title: '내 체험 관리' },
-  { id: 4, icon: <Setting />, title: '예약 현황' },
+export type NavState = 'profile' | 'history' | 'experiences' | 'status';
+
+interface NavItem {
+  id: number;
+  icon: JSX.Element;
+  title: string;
+  state: NavState;
+}
+
+interface SideNavigationMenuProps {
+  onNavClick: (state: NavState) => void;
+}
+
+const navItems: NavItem[] = [
+  { id: 1, icon: <AccountCheck />, title: '내 정보', state: 'profile' },
+  { id: 2, icon: <TextBoxCheck />, title: '예약 내역', state: 'history' },
+  { id: 3, icon: <CalendarCheck />, title: '내 체험 관리', state: 'experiences' },
+  { id: 4, icon: <Setting />, title: '예약 현황', state: 'status' },
 ];
 
-export default function SideNavigationMenu() {
+export default function SideNavigationMenu({ onNavClick }: SideNavigationMenuProps) {
   const cn = classNames.bind(styles);
 
+  const handleNavClick = (state: NavState) => {
+    onNavClick(state);
+  };
+
   return (
-    <div className={cn(styles.sideNavigationMenu)}>
-      <div className={cn(styles.profileImgWrapper)}>
-        <div className={cn(styles.profileImgContainer)}>
+    <div className={cn('sideNavigationMenu')}>
+      <div className={cn('profileImgWrapper')}>
+        <div className={cn('profileImgContainer')}>
           <Image src={ProfileImg} alt="프로필 이미지" />
-          <div className={cn(styles.penWrapper)}>
+          <div className={cn('penWrapper')}>
             <Pen />
           </div>
         </div>
       </div>
-      <div className={cn(styles.navMenuList)}>
+      <div className={cn('navMenuList')}>
         {navItems.map((item) => (
-          <div key={item.id} className={cn(styles.navMenu)}>
-            <div className={cn(styles.navIcon)}>{item.icon}</div>
-            <div className={cn(styles.navTitle)}>{item.title}</div>
+          <div key={item.id} className={cn('navMenu')} onClick={() => handleNavClick(item.state)}>
+            <div className={cn('navIcon')}>{item.icon}</div>
+            <div className={cn('navTitle')}>{item.title}</div>
           </div>
         ))}
       </div>
