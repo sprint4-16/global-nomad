26c9ef64902d5a201fad3b7bd3b48cfce30ef51e
youth-o <127082479+youth-o@users.noreply.github.com>
2024-05-22 03:04:15 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,6 @@ export default function Home() {
   return (
     <>
       랜딩페이지
-      <Input label={'이메일'} type={'email'} placeholder={'이메일을 작성해주세요'} />
-      <Input label={'비밀번호'} type={'password'} placeholder={'비밀번호를 작성해주세요'} />
-      <Dropdown buttonText={'카테고리'} />
-      <DateInput dateText={'YY/MM/DD'} />
     </>
   );
 }
