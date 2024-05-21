17434dacab486001a9f40ab69e9a454de8bb89c8
Youth-o <2021111495@dgu.ac.kr>
2024-05-21 13:48:49 +0900

    feat: DateInput 컴포넌트 UI 제작

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,26 +3,35 @@ import EyeOff from '@/images/btn/btn_eye_off.svg';
 import EyeOn from '@/images/btn/btn_eye_on.svg';
 import ArrowDown from '@/images/btn/btn_chevron_down.svg';
 import ArrowUp from '@/images/btn/btn_chevron_up.svg';
+import Calendar from '@/images/icon/icon_calendar.svg';
 import styles from './Input.module.scss';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
 interface InputProps {
-  label: ReactNode;
+  label?: ReactNode;
   type: React.HTMLInputTypeAttribute;
   placeholder?: string;
   color?: string;
   className?: string;
+  onClick?: () => void;
 }
 
 interface DropdownProps {
   buttonText?: string;
   menuItems?: string[];
   className?: string;
+  onClick?: () => void;
 }
 
-export function Input({ label, type, placeholder, color, className }: InputProps) {
+interface DateInputProps {
+  dateText?: string;
+  className?: string;
+  onClick?: () => void;
+}
+
+export function Input({ label, type, placeholder, color, onClick, className }: InputProps) {
   return (
     <div className={cx('input-container', className)}>
       <label htmlFor={type} className={cx('label')}>
@@ -40,12 +49,23 @@ export function Input({ label, type, placeholder, color, className }: InputProps
   );
 }
 
-export function Dropdown({ buttonText, menuItems, className }: DropdownProps) {
+export function Dropdown({ buttonText, menuItems, onClick, className }: DropdownProps) {
   return (
     <div className={cx('dropdown-wrapper', className)}>
       <div className={cx('dropdown-box')}>
         <button className={cx('dropdown-button')}>{buttonText}</button>
         <ArrowDown className={cx('arrow-img')} />
+      </div>
+    </div>
+  );
+}
+
+export function DateInput({ dateText, onClick, className }: DateInputProps) {
+  return (
+    <div className={cx('date-input-wrapper', className)}>
+      <div className={cx('date-input-box')}>
+        {dateText}
+        <Calendar className={cx('calendar-img')} />
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Dropdown, Input } from '@/components/Input/Input';
+import { DateInput, Dropdown, Input } from '@/components/Input/Input';
 
 export default function Home() {
   return (
@@ -7,6 +7,7 @@ export default function Home() {
       <Input label={'이메일'} type={'email'} placeholder={'이메일을 작성해주세요'} />
       <Input label={'비밀번호'} type={'password'} placeholder={'비밀번호를 작성해주세요'} />
       <Dropdown buttonText={'카테고리'} />
+      <DateInput dateText={'YY/MM/DD'} />
     </>
   );
 }
