a027bd492cb1a8ea0ddf5a5d77e10759bdadf9cb
Youth-o <2021111495@dgu.ac.kr>
2024-05-22 04:32:16 +0900

    style: Chips 컴포넌트 스타일 적용

--- src/components/Chips/Chips.tsx
+++ src/components/Chips/Chips.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 import style from './Chips.module.scss';
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
 import { CSSProperties } from 'react';
 
 const cn = classNames.bind(style);
@@ -11,12 +11,33 @@ interface ChipsProps {
   children: ReactNode;
   className?: string;
   size: Size;
-  color?: CSSProperties;
+  color?: string;
 }
 
 export function Chips({ children, className, size, color }: ChipsProps) {
+  const getFontColor = (bgColor: string | undefined): string => {
+    if (!bgColor) return '#000';
+
+    switch (bgColor.toLowerCase()) {
+      case '#ffffff':
+        return '#0085ff';
+      case '#0085ff':
+        return '#fff';
+      case '#dddddd':
+        return '#4b4b4b';
+      case '#fff4e8':
+        return '#ff7c1d';
+      default:
+        return '#000';
+    }
+  };
+  const chipStyle: CSSProperties = {
+    backgroundColor: color,
+    color: getFontColor(color),
+  };
+
   return (
-    <div className={cn('chips', size, className)} style={color}>
+    <div className={cn('chips', size, className)} style={chipStyle}>
       {children}
     </div>
   );
