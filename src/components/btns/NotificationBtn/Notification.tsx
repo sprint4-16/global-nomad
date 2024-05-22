fdb48ee58a793753ae317872dd073f5dcf93f09c
Taejin Kim <rlaxowls1316@naver.com>
2024-05-22 12:41:33 +0900

    feat: NotificationBtn 컴포넌트 구현

---
+++ src/components/btns/NotificationBtn/Notification.tsx
@@ -0,0 +1,14 @@
+import NotificationBtnIcon from '../../../../public/btn/btn_notification.svg';
+
+interface NotificationProps {
+  onClick?: () => void;
+  size?: number;
+}
+
+export default function NotificationBtn({ onClick, size = 24 }: NotificationProps) {
+  return (
+    <button onClick={onClick}>
+      <NotificationBtnIcon width={size} height={size} />
+    </button>
+  );
+}
