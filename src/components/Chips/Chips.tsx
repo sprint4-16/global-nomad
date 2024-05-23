aa14fafaeaa5fa294b9a1bce49717cc932b533a4
Youth-o <2021111495@dgu.ac.kr>
2024-05-23 14:53:52 +0900

    style: type으로 background-color, font color 관리

--- src/components/Chips/Chips.tsx
+++ src/components/Chips/Chips.tsx
@@ -6,39 +6,15 @@ import { CSSProperties } from 'react';
 const cn = classNames.bind(style);
 
 type Size = 'large' | 'medium' | 'small';
+type ChipType = 'seat' | 'reservation' | 'complete' | 'confirmed';
 
 interface ChipsProps {
   children: ReactNode;
   className?: string;
   size: Size;
-  color?: string;
+  type: ChipType;
 }
 
-export function Chips({ children, className, size, color }: ChipsProps) {
-  const getFontColor = (bgColor: string | undefined): string => {
-    if (!bgColor) return '#000';
-
-    switch (bgColor.toLowerCase()) {
-      case '#ffffff':
-        return '#0085ff';
-      case '#0085ff':
-        return '#fff';
-      case '#dddddd':
-        return '#4b4b4b';
-      case '#fff4e8':
-        return '#ff7c1d';
-      default:
-        return '#000';
-    }
-  };
-  const chipStyle: CSSProperties = {
-    backgroundColor: color,
-    color: getFontColor(color),
-  };
-
-  return (
-    <div className={cn('chips', size, className)} style={chipStyle}>
-      {children}
-    </div>
-  );
+export function Chips({ children, className, size, type }: ChipsProps) {
+  return <div className={cn('chips', size, className, type)}>{children}</div>;
 }
