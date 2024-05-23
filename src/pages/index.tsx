8a9829c22e55f2d005976ab34850ee1cfba2775f
Youth-o <2021111495@dgu.ac.kr>
2024-05-23 14:38:20 +0900

    style: box-shadow opacity 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,3 @@
-import { Dropdown, menuItem } from '@/components/Dropdown/Dropdown';
-import { useState } from 'react';
-
 export default function Home() {
-  const [selectedItem, setSelectedItem] = useState<menuItem | null>(null);
-
-  const handleSelect = (item: menuItem) => {
-    setSelectedItem(item);
-  };
-
-  return (
-    <>
-      랜딩페이지
-      <h1>Selected Item: {selectedItem ? selectedItem.itemText : 'None'}</h1>
-      <Dropdown onSelect={handleSelect} />
-    </>
-  );
+  return <>랜딩페이지</>;
 }
