c71676f8e98ae2687e0bd6c719a73dc4ef600477
성욱 <155031984+SeongWook-Hong@users.noreply.github.com>
2024-05-23 16:36:42 +0900

    chore: index.tsx 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,3 @@
-import Category from '@/components/Category&Filter/Category/Category';
-import Filter from '@/components/Category&Filter/Filter/Filter';
-import Pagination from '@/components/Pagination/Pagination';
-
 export default function Home() {
-  const TEMP_LIST = ['스포츠', '식음료', '투어', '웰빙', '문화 · 예술'];
-  return (
-    <>
-      <div>\n</div>
-      <Category list={TEMP_LIST} />
-      <Filter />
-      <Pagination total={12} />
-    </>
-  );
+  return <>랜딩페이지</>;
 }
