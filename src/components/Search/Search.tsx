3dc3b9e102fec1990b04e6405ac350b8394545ce
Youth-o <2021111495@dgu.ac.kr>
2024-05-24 23:13:54 +0900

    feat: Search 컴포넌트 검색 기능 추가

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -2,7 +2,7 @@ import styles from './Search.module.scss';
 import classNames from 'classnames/bind';
 import Icon from '@/images/icon/icon_bed.svg';
 import Button from '../Button/Button';
-import { CSSProperties } from 'react';
+import { CSSProperties, useState } from 'react';
 
 const cn = classNames.bind(styles);
 
@@ -14,11 +14,25 @@ interface SearchProps {
   size: Size;
   sx?: CSSProperties;
   className?: string;
+  data?: any[]; // 어떤 타입이 올지 몰라서 일단 any로 뒀습니다!
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  onClick?: (filteredResults: any[]) => void;
 }
 
-export function Search({ titleText, inputText, size, sx, className }: SearchProps) {
+export function Search({ titleText, inputText, size, sx, className, data = [], onChange, onClick }: SearchProps) {
   const buttonStyle: CSSProperties = {
     padding: '1.4rem 2rem',
+  };
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+    if (onChange) onChange(e);
+  };
+
+  const handleSearchClick = () => {
+    const results = data.filter((item) => item.toLowerCase().includes(searchTerm.toLowerCase()));
+    if (onClick) onClick(results);
   };
 
   return (
@@ -27,7 +41,14 @@ export function Search({ titleText, inputText, size, sx, className }: SearchProp
       <div className={cn('searchBox')}>
         <Icon className={cn('bedImg')} />
         <div className={cn('inputContainer')}>
-          <input className={cn('input')} type="text" id="searchInput" placeholder=" " />
+          <input
+            className={cn('input')}
+            type="text"
+            id="searchInput"
+            placeholder=" "
+            value={searchTerm}
+            onChange={handleInputChange}
+          />
           <label className={cn('placeholder')} htmlFor="searchInput">
             {inputText}
           </label>
@@ -37,7 +58,7 @@ export function Search({ titleText, inputText, size, sx, className }: SearchProp
           size={size === 'small' ? 'small' : 'medium'}
           sx={size === 'small' ? buttonStyle : undefined}
           className={cn('searchButton')}
-          onClick={() => console.log('Button clicked!')}
+          onClick={handleSearchClick}
         >
           검색하기
         </Button>
