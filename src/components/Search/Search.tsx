4f085f99ca6d7c9c00f0e2a9475337b3473d12ac
youth-o <127082479+youth-o@users.noreply.github.com>
2024-05-25 22:59:25 +0900

    style: 반응형 수정

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -3,15 +3,13 @@ import classNames from 'classnames/bind';
 import Icon from '@/images/icon/icon_bed.svg';
 import Button from '../Button/Button';
 import { CSSProperties, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 const cn = classNames.bind(styles);
-
-type Size = 'large' | 'medium' | 'small';
 
 interface SearchProps {
   titleText?: string;
   inputText?: string;
-  size: Size;
   sx?: CSSProperties;
   className?: string;
   data?: any[]; // 어떤 타입이 올지 몰라서 일단 any로 뒀습니다!
@@ -19,11 +17,12 @@ interface SearchProps {
   onClick?: (filteredResults: any[]) => void;
 }
 
-export function Search({ titleText, inputText, size, sx, className, data = [], onChange, onClick }: SearchProps) {
+export function Search({ titleText, inputText, sx, className, data = [], onChange, onClick }: SearchProps) {
   const buttonStyle: CSSProperties = {
     padding: '1.4rem 2rem',
   };
   const [searchTerm, setSearchTerm] = useState('');
+  const isMobile = useMediaQuery({ query: '(max-width: 375px)' });
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value);
@@ -36,32 +35,34 @@ export function Search({ titleText, inputText, size, sx, className, data = [], o
   };
 
   return (
-    <div className={cn('searchContainer', size, className)} style={sx}>
-      <div className={cn('searchLabel')}>{titleText}</div>
-      <div className={cn('searchBox')}>
-        <Icon className={cn('bedImg')} />
-        <div className={cn('inputContainer')}>
-          <input
-            className={cn('input')}
-            type="text"
-            id="searchInput"
-            placeholder=" "
-            value={searchTerm}
-            onChange={handleInputChange}
-          />
-          <label className={cn('placeholder')} htmlFor="searchInput">
-            {inputText}
-          </label>
+    <div className={cn('wrapper')}>
+      <div className={cn('container', className)} style={sx}>
+        <div className={cn('searchLabel')}>{titleText}</div>
+        <div className={cn('searchBox')}>
+          <Icon className={cn('bedImg')} />
+          <div className={cn('inputContainer')}>
+            <input
+              className={cn('input')}
+              type="text"
+              id="searchInput"
+              placeholder=" "
+              value={searchTerm}
+              onChange={handleInputChange}
+            />
+            <label className={cn('placeholder')} htmlFor="searchInput">
+              {inputText}
+            </label>
+          </div>
+          <Button
+            type="primary"
+            size={isMobile ? 'small' : 'medium'}
+            sx={isMobile ? buttonStyle : undefined}
+            className={cn('searchButton')}
+            onClick={handleSearchClick}
+          >
+            검색하기
+          </Button>
         </div>
-        <Button
-          type="primary"
-          size={size === 'small' ? 'small' : 'medium'}
-          sx={size === 'small' ? buttonStyle : undefined}
-          className={cn('searchButton')}
-          onClick={handleSearchClick}
-        >
-          검색하기
-        </Button>
       </div>
     </div>
   );
