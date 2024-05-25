a7db706915159777ea15a6e24472adc0f6f6b065
hong <hsw5971@naver.com>
2024-05-25 14:31:10 +0900

    style: 클래스명 변경

--- src/components/FloatingBox/DateInfo.tsx
+++ src/components/FloatingBox/DateInfo.tsx
@@ -1,7 +1,6 @@
 import classNames from 'classnames/bind';
-import Stroke from '@/images/icon/icon_stroke.svg';
-
 import styles from './FloatingBox.module.scss';
+import Stroke from '@/images/icon/icon_stroke.svg';
 
 const cn = classNames.bind(styles);
 
@@ -13,9 +12,9 @@ export default function DateInfo() {
       <div className={cn('selectDate')}>날짜 선택하기</div>
       <div className={cn('reservationTime')}>
         <div className={cn('mainText')}>예약 가능한 시간</div>
-        <div className={cn('tempBtns')}>
-          <div className={cn('btn1')}>14:00~15:00</div>
-          <div className={cn('btn2')}>15:00~16:00</div>
+        <div className={cn('timeBtns')}>
+          <div className={cn('btn')}>14:00~15:00</div>
+          <div className={cn('btnClicked')}>15:00~16:00</div>
         </div>
       </div>
       <Stroke width={336} className={cn('stroke')} />
