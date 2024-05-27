a88ca3882c19c16ffdd334adac9977ab71164fc1
김동현 <dtc03003@naver.com>
2024-05-27 14:32:00 +0900

    feat: ReservationDetailCard 컴포넌트 구현

---
+++ src/components/Card/ReservationDetailCard.tsx
@@ -0,0 +1,59 @@
+import classNames from 'classnames/bind';
+import Button from '../Button/Button';
+import styles from './ReservationDetailCard.module.scss';
+import { Chips } from '../Chips/Chips';
+
+type ReservationState = 'notReserved' | 'confirmed' | 'rejected';
+
+interface Props {
+  nickname: string;
+  people: number;
+  reservationState: ReservationState;
+}
+
+export default function ReservationDetailCard({ nickname, people, reservationState }: Props) {
+  const cn = classNames.bind(styles);
+
+  const handleConfirmClick = () => {
+    console.log('확정하기');
+  };
+
+  const handleRejectedClick = () => {
+    console.log('거절하기');
+  };
+
+  return (
+    <div className={cn('reservationDetailCard')}>
+      <div className={cn('infoContainer')}>
+        <div className={cn('info')}>
+          닉네임 <p className={cn('bold')}>{nickname}</p>
+        </div>
+        <div className={cn('info')}>
+          인원 <p className={cn('bold')}>{people}명</p>
+        </div>
+      </div>
+      <div className={cn('footer')}>
+        {reservationState == 'notReserved' && (
+          <>
+            <Button className={cn('button')} type="primary" size="small" onClick={handleConfirmClick}>
+              확정하기
+            </Button>
+            <Button className={cn('button')} type="secondary" size="small" onClick={handleRejectedClick}>
+              거절하기
+            </Button>
+          </>
+        )}
+        {reservationState == 'confirmed' && (
+          <Chips className={cn('chips', 'confirmed')} size="medium" type="confirmed">
+            예약확정
+          </Chips>
+        )}
+        {reservationState == 'rejected' && (
+          <Chips className={cn('chips', 'rejected')} size="medium" type="confirmed">
+            예약거절
+          </Chips>
+        )}
+      </div>
+    </div>
+  );
+}
