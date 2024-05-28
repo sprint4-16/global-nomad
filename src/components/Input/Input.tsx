9a9a34c077dc097ed604efb6463aedf7ae827641
Youth-o <2021111495@dgu.ac.kr>
2024-05-28 14:10:05 +0900

    .

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,8 +1,9 @@
-import { CSSProperties, ReactNode, useState } from 'react';
+import { CSSProperties, ReactNode, forwardRef, useState } from 'react';
 import EyeOff from '@/images/btn/btn_eye_off.svg';
 import EyeOn from '@/images/btn/btn_eye_on.svg';
 import styles from './Input.module.scss';
 import classNames from 'classnames/bind';
+import { UseFormRegisterReturn } from 'react-hook-form';
 
 const cn = classNames.bind(styles);
 
@@ -15,22 +16,40 @@ interface InputProps {
   className?: string;
   labelClassName?: string;
   onClick?: () => void;
+  register?: UseFormRegisterReturn;
 }
 
-export function Input({ label, type, placeholder, color, sx, onClick, className, labelClassName }: InputProps) {
-  return (
-    <div className={cn('inputContainer', className)}>
-      <label htmlFor={type} className={cn('label', labelClassName)}>
-        {label}
-      </label>
-      {type === 'password' ? (
-        <>
-          <input type={type} id={type} placeholder={placeholder} className={cn('input', color)} style={sx} />
-          <EyeOff className={cn('eyeImg')} onClick={onClick} />
-        </>
-      ) : (
-        <input type={type} id={type} placeholder={placeholder} className={cn('input', color)} />
-      )}
-    </div>
-  );
-}
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, type, placeholder, color, sx, onClick, className, labelClassName, register, ...props }, ref) => {
+    return (
+      <div className={cn('inputContainer', className)}>
+        <label htmlFor={type} className={cn('label', labelClassName)}>
+          {label}
+        </label>
+        {type === 'password' ? (
+          <>
+            <input
+              type={type}
+              id={type}
+              placeholder={placeholder}
+              className={cn('input', color)}
+              style={sx}
+              ref={ref}
+              {...register}
+            />
+            <EyeOff className={cn('eyeImg')} onClick={onClick} />
+          </>
+        ) : (
+          <input
+            type={type}
+            id={type}
+            placeholder={placeholder}
+            className={cn('input', color)}
+            ref={ref}
+            {...register}
+          />
+        )}
+      </div>
+    );
+  },
+);
--- src/components/MyPage/MyPageForm/MyPageForm.tsx
+++ src/components/MyPage/MyPageForm/MyPageForm.tsx
@@ -2,11 +2,33 @@ import Button from '@/components/Button/Button';
 import styles from './MyPageForm.module.scss';
 import classNames from 'classnames/bind';
 import { Input } from '@/components/Input/Input';
-import { CSSProperties } from 'react';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 const cn = classNames.bind(styles);
 
 export default function MyPageForm() {
+  const formSchema = yup.object({
+    nickname: yup.string().max(10, '열 자 이하로 작성해 주세요.'),
+    password: yup
+      .string()
+      .required('8자 이상 입력해 주세요.')
+      .min(8, '8자 이상 입력해 주세요.')
+      .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/, '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.'),
+    newPassword: yup.string().oneOf([yup.ref('password')], '비밀번호가 일치하지 않습니다.'),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: 'onBlur',
+    //@ts-ignore
+    resolver: yupResolver(formSchema) as Resolver,
+  });
+
   return (
     <form>
       <div className={cn('titleBox')}>
@@ -16,15 +38,31 @@ export default function MyPageForm() {
         </Button>
       </div>
       <div className={cn('formContainer')}>
-        <Input label="닉네임" type="text" placeholder="정만철" labelClassName={cn('customLabel')} />
+        <Input
+          label="닉네임"
+          type="text"
+          placeholder="정만철"
+          labelClassName={cn('customLabel')}
+          register={register('nickname')}
+        />
+        {errors.nickname && <div className={cn('error')}>{errors.nickname.message as string}</div>}
         <Input label="이메일" type="email" placeholder="12345@example.com" labelClassName={cn('customLabel')} />
-        <Input label="비밀번호" type="text" placeholder="8자 이상 입력해 주세요" labelClassName={cn('customLabel')} />
+        <Input
+          label="비밀번호"
+          type="text"
+          placeholder="8자 이상 입력해 주세요"
+          labelClassName={cn('customLabel')}
+          register={register('password')}
+        />
+        {errors.password && <div className={cn('error')}>{errors.password.message as string}</div>}
         <Input
           label="비밀번호 확인"
           type="text"
           placeholder="비밀번호를 한 번 더 입력해 주세요"
           labelClassName={cn('customLabel')}
+          register={register('newPassword')}
         />
+        {errors.newPassword && <div className={cn('error')}>{errors.newPassword.message as string}</div>}
       </div>
     </form>
   );
