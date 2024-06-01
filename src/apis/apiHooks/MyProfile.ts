6ff320674a9fd155c44bd36d4b654b7a73bec8bb
Youth-o <2021111495@dgu.ac.kr>
2024-06-02 00:09:43 +0900

    feat: useUploadProfileImage Hook 추가

--- src/apis/apiHooks/MyProfile.ts
+++ src/apis/apiHooks/MyProfile.ts
@@ -1,10 +1,9 @@
-// MyProfile.ts 파일에서 수정
-
 import { useQuery, useMutation } from '@tanstack/react-query';
 import { axiosInstanceToken } from '../axiosInstance';
 import { END_POINT } from '@/constants/';
 import useGetCookie from '@/hooks/useCookies';
 
+// 로그인한 유저의 프로필 정보 가져오는 API
 export function useGetProfile() {
   const { getCookie } = useGetCookie();
   const accessToken = getCookie('accessToken');
@@ -21,12 +20,44 @@ export function useGetProfile() {
   });
 }
 
+interface UseUploadProfileImageProps {
+  file: File;
+}
+
+export function useUploadProfileImage() {
+  const { getCookie } = useGetCookie();
+
+  return useMutation({
+    mutationFn: async ({ file }: UseUploadProfileImageProps) => {
+      const accessToken = getCookie('accessToken');
+
+      if (!accessToken) throw new Error('Access token is not available');
+
+      const formData = new FormData();
+      formData.append('image', file);
+
+      const { data } = await axiosInstanceToken(accessToken).post(
+        `${END_POINT.MY_RESERVATIONS}/users/me/image`,
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        },
+      );
+
+      return data.profileImageUrl;
+    },
+  });
+}
+
 export interface usePatchProfileProps {
   nickname: string;
   newPassword: string;
   profileImageUrl: string;
 }
 
+// 유저의 프로필 정보를 수정하는 API
 export function usePatchProfile() {
   const { getCookie } = useGetCookie();
 
