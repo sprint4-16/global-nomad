5871c3c283811fe39a7e83d9487233afe6acbf0d
김동현 <dtc03003@naver.com>
2024-06-04 23:07:28 +0900

    feat: React Query 사용

--- src/pageLayouts/ActivityLayout/ActivityLayout.tsx
+++ src/pageLayouts/ActivityLayout/ActivityLayout.tsx
@@ -1,12 +1,11 @@
-import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import classNames from 'classnames/bind';
 
-import { axiosInstanceToken } from '@/apis/axiosInstance';
+import { ROUTE } from '@/constants';
+import { useGetMyActivities } from '@/apis/apiHooks/MyReservations';
 import ExperienceCard from '@/pages/card/ExperienceCard';
 import Button from '@/components/Button/Button';
 import styles from './ActivityLayout.module.scss';
-import { ROUTE } from '@/constants';
 
 interface CardProps {
   id: number;
@@ -20,25 +19,10 @@ interface CardProps {
 export default function ActivityLayout() {
   const router = useRouter();
   const cn = classNames.bind(styles);
-  const [activities, setActivities] = useState<CardProps[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const accessToken =
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzQxLCJ0ZWFtSWQiOiI0LTE2IiwiaWF0IjoxNzE3NDcxNzc4LCJleHAiOjE3MTc0NzM1NzgsImlzcyI6InNwLWdsb2JhbG5vbWFkIn0.XmGLc4d-9J0NuIG0c3mG-59tA7qZxgMC2CmQBkS9XY8';
-        const { data } = await axiosInstanceToken(accessToken).get(`my-activities?size=20`);
-        setActivities(data.activities);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-    fetchData();
-  }, []);
+  const { data, isLoading } = useGetMyActivities();
 
   const handleClickAddActivity = () => {
-    // 체험 등록하기
-    router.push(`/${ROUTE.ACTIVITY_POST}`);
+    router.push(`${ROUTE.ACTIVITY_POST}`);
   };
 
   return (
@@ -50,9 +34,11 @@ export default function ActivityLayout() {
         </Button>
       </div>
       <div className={cn('cardList')}>
-        {activities.map((item: CardProps) => {
-          return <ExperienceCard key={item.id} cardData={item} />;
-        })}
+        {isLoading ? (
+          <div>로딩중</div>
+        ) : (
+          data.activities.map((item: CardProps) => <ExperienceCard key={item.id} cardData={item} />)
+        )}
       </div>
     </div>
   );
