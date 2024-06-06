e1e3dfdedfe5cf5e435df3cf5e87e48223470e6a
Youth-o <2021111495@dgu.ac.kr>
2024-06-06 20:13:41 +0900

    fix: 버튼 클릭시 form 제출 기능 막기

--- src/components/btns/AddImageBtn/AddImageBtn.tsx
+++ src/components/btns/AddImageBtn/AddImageBtn.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import AddImageIcon from '@/images/btn/btn_add_img.svg';
 
 interface AddImageBtnProps {
@@ -6,8 +7,15 @@ interface AddImageBtnProps {
 }
 
 export default function AddImageBtn({ onClick, size = 180 }: AddImageBtnProps) {
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault();
+    if (onClick) {
+      onClick();
+    }
+  };
+
   return (
-    <button onClick={onClick}>
+    <button onClick={handleClick}>
       <AddImageIcon width={size} height={size} />
     </button>
   );
