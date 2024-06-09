c2809cdd39b401e98241148f52ec33eca315d5eb
hong <hsw5971@naver.com>
2024-06-09 23:45:14 +0900

    feat: 예약 내역 페이지 무한 스크롤 기능 구현

--- src/pages/user/reservations/index.tsx
+++ src/pages/user/reservations/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useGetReservation } from '@/apis/apiHooks/MyReservations';
 
 import Filter from '@/components/Category&Filter/Filter/Filter';
@@ -12,8 +12,27 @@ const cn = classNames.bind(styles);
 
 export default function ReservationsPage() {
   const [filterStatus, setFilterStatus] = useState<string | undefined>(undefined);
-  const { data, isFetching } = useGetReservation(filterStatus);
-  const reservations = data?.reservations ?? [];
+  const { data, isLoading, fetchNextPage, hasNextPage } = useGetReservation(filterStatus);
+  const reservations = data?.pages.flatMap((page) => page.reservations) ?? [];
+
+  const observerRef = useRef(null);
+  const handleIntersection: IntersectionObserverCallback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && hasNextPage) {
+        fetchNextPage();
+      }
+    });
+  };
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleIntersection);
+    if (observerRef.current) {
+      observer.observe(observerRef.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [reservations]);
 
   return (
     <div className={cn('container')}>
@@ -22,14 +41,21 @@ export default function ReservationsPage() {
         <Filter filterType="reservation" setFilterStatus={setFilterStatus} />
       </div>
       <div className={cn('content')}>
-        {isFetching || reservations.length === 0 ? (
+        {isLoading || reservations.length === 0 ? (
           <div className={cn('content_empty')}>
             <EmptyIcon width="24rem" height="24rem" />
             <div className={cn('content_text')}>아직 등록한 체험이 없어요</div>
           </div>
         ) : (
           reservations.map((item, index: number) => {
-            return <ReservationCard className={''} cardData={item} key={`${index} ${item.activity.title}`} />;
+            return (
+              <ReservationCard
+                className={''}
+                cardData={item}
+                key={`${index} ${item.activity.title}`}
+                ref={index === reservations.length - 1 ? observerRef : null}
+              />
+            );
           })
         )}
       </div>
