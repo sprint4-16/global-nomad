a3cdf3ae556092ad923535c9b012b1bb33cdc6b7
hong <hsw5971@naver.com>
2024-06-10 23:31:38 +0900

    feat: 모달 생성 시 scroll block 관련 hook 추가

---
+++ src/hooks/useBlockScroll.ts
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+
+const blockScroll = () => {
+  const currentScrollY = window.scrollY;
+  document.body.style.position = 'fixed';
+  document.body.style.width = '100%';
+  document.body.style.height = '100%';
+  document.body.style.top = `-${currentScrollY}px`;
+  document.body.style.overflowY = 'scroll';
+  console.log(currentScrollY);
+  return currentScrollY;
+};
+
+const allowScroll = (prevScrollY: number) => {
+  document.body.style.position = '';
+  document.body.style.width = '';
+  document.body.style.height = '';
+  document.body.style.top = '';
+  document.body.style.overflowY = '';
+  window.scrollTo(0, prevScrollY);
+};
+
+const useBlockScroll = () => {
+  useEffect(() => {
+    const prevScrollY = blockScroll();
+    return () => {
+      allowScroll(prevScrollY);
+    };
+  }, []);
+};
+
+export default useBlockScroll;
