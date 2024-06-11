8476a194fe5636ae872e95aa5332c53fc6fcd2d2
Taejin Kim <rlaxowls1316@naver.com>
2024-06-11 13:47:33 +0900

    fix: _app.tsx 라우팅 처리 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,17 +5,17 @@ import type { AppProps } from 'next/app';
 import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 
-// import { GNB_REQUIRES, SIDE_NAV_MENU_REQUIRES } from '@/constants/index';
-// import GlobalNavigationBar from '@/components/GlobalNavigationBar/GlobalNavigationBar';
-// import Footer from '@/components/Footer/Footer';
-// import SideNavigationMenuLayout from '@/pageLayouts/commonLayouts/SideNavigationMenuLayout/SideNavigationMenuLayout';
+import { GNB_REQUIRES, SIDE_NAV_MENU_REQUIRES } from '@/constants/index';
+import GlobalNavigationBar from '@/components/GlobalNavigationBar/GlobalNavigationBar';
+import Footer from '@/components/Footer/Footer';
+import SideNavigationMenuLayout from '@/pageLayouts/commonLayouts/SideNavigationMenuLayout/SideNavigationMenuLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
-  // const router = useRouter();
-  // const checkRouteInGNB = GNB_REQUIRES.includes(router.pathname);
-  // const checkRouteInSideNavMenu = SIDE_NAV_MENU_REQUIRES.includes(router.pathname);
+  const router = useRouter();
+  const checkRouteInGNB = GNB_REQUIRES.includes(router.pathname);
+  const checkRouteInSideNavMenu = SIDE_NAV_MENU_REQUIRES.includes(router.pathname);
 
   const [queryClient] = useState(
     () =>
@@ -28,24 +28,29 @@ export default function App({ Component, pageProps }: AppProps) {
       }),
   );
 
-  // const contentStyle = checkRouteInGNB ? { marginTop: '7rem' } : {};
+  const contentStyle = checkRouteInGNB ? { marginTop: '7rem' } : {};
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <div id="wrapper"> */}
-      <div id="modal-root" />
-      {/* {checkRouteInGNB && <GlobalNavigationBar />} */}
-      {/* <div id="contentWrapper" style={contentStyle}> */}
-      {/* {checkRouteInSideNavMenu ? ( */}
-      {/* <SideNavigationMenuLayout> */}
-      {/* <Component {...pageProps} /> */}
-      {/* </SideNavigationMenuLayout> */}
-      {/* ) : ( */}
-      <Component {...pageProps} />
-      {/* )} */}
-      {/* </div> */}
-      {/* {checkRouteInGNB && <Footer />} */}
-      {/* </div> */}
+      {checkRouteInGNB ? (
+        <div id="wrapper">
+          <div id="modal-root" />
+          <GlobalNavigationBar />
+          <div id="contentWrapper" style={contentStyle}>
+            {checkRouteInSideNavMenu ? (
+              <SideNavigationMenuLayout>
+                <Component {...pageProps} />
+              </SideNavigationMenuLayout>
+            ) : (
+              <Component {...pageProps} />
+            )}
+          </div>
+          <Footer />
+        </div>
+      ) : (
+        <Component {...pageProps} />
+      )}
+
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
