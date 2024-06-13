bd6ec2cc9b1ffd571ba12c8b177c9f01f3d3b6c0
Taejin Kim <rlaxowls1316@naver.com>
2024-06-13 17:18:11 +0900

    feat: header skeleton ui적용

--- src/pages/activities/[activityId].tsx
+++ src/pages/activities/[activityId].tsx
@@ -5,31 +5,35 @@ import classNames from 'classnames/bind';
 import KebabBtn from '@/components/btns/KebabBtn/KebabBtn';
 import StarIcon from '@/images/icon/icon_star_on.svg';
 import LocationIcon from '@/images/icon/icon_location.svg';
+import Header from './_skeleton-ui/skeleton-header';
 
 const cn = classNames.bind(styles);
 
 export default function Activity() {
   const router = useRouter();
-  const { data } = useGetActivity({ activityId: router.query.activityId?.toString() ?? '' });
+  const { data, isLoading } = useGetActivity({ activityId: router.query.activityId?.toString() ?? '' });
 
   return (
     <>
-      <header className={cn('header')}>
-        <div className={cn('header-contents')}>
-          <div className={cn('tag')}>{data?.category}</div>
-          <h2 className={cn('title')}>{data?.title}</h2>
-          <div className={cn('footer')}>
-            <span className={cn('rating')}>
-              <StarIcon width={15} height={22} /> <span>({data?.reviewCount})</span>
-            </span>
-            <span className={cn('address')}>
-              <LocationIcon height={18} />
-              <span>{data?.address}</span>
-            </span>
+      {isLoading && <Header />}
+      {!isLoading && (
+        <header className={cn('header')}>
+          <div className={cn('header-contents')}>
+            <div className={cn('category')}>{data?.category}</div>
+            <h2 className={cn('title')}>{data?.title}</h2>
+            <div className={cn('footer')}>
+              <span className={cn('rating')}>
+                <StarIcon width={15} height={22} /> <span>({data?.reviewCount})</span>
+              </span>
+              <span className={cn('address')}>
+                <LocationIcon height={18} />
+                <span>{data?.address}</span>
+              </span>
+            </div>
           </div>
-        </div>
-        <KebabBtn size={28} />
-      </header>
+          <KebabBtn size={28} />
+        </header>
+      )}
     </>
   );
 }
