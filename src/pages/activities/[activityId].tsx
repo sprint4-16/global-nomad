0d9c3fec4a4940e257d3759bc76218dd5ba35420
Taejin Kim <rlaxowls1316@naver.com>
2024-06-13 14:24:02 +0900

    feat: 체험상세 페이지 header ui 완성

--- src/pages/activities/[activityId].tsx
+++ src/pages/activities/[activityId].tsx
@@ -4,6 +4,7 @@ import styles from './Activity.module.scss';
 import classNames from 'classnames/bind';
 import KebabBtn from '@/components/btns/KebabBtn/KebabBtn';
 import StarIcon from '@/images/icon/icon_star_on.svg';
+import LocationIcon from '@/images/icon/icon_location.svg';
 
 const cn = classNames.bind(styles);
 
@@ -16,16 +17,19 @@ export default function Activity() {
     <>
       <header className={cn('header')}>
         <div className={cn('header-contents')}>
-          <div className={cn('header')}>{data?.category}</div>
+          <div className={cn('tag')}>{data?.category}</div>
           <h2 className={cn('title')}>{data?.title}</h2>
           <div className={cn('footer')}>
-            <span>
-              <StarIcon width={20} height={40} /> ({data?.reviewCount})
+            <span className={cn('rating')}>
+              <StarIcon width={15} height={22} /> <span>({data?.reviewCount})</span>
             </span>
-            <span>{data?.address}</span>
+            <span className={cn('address')}>
+              <LocationIcon height={18} />
+              <span>{data?.address}</span>
+            </span>
           </div>
         </div>
-        <KebabBtn size={30} />
+        <KebabBtn size={28} />
       </header>
     </>
   );
