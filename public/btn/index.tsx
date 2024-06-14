4a325014ed0b75160ae3e4af81054364e70ac2db
김동현 <dtc03003@naver.com>
2024-06-15 08:13:09 +0900

    chore: 이미지 인덱스 추가

---
+++ public/btn/index.tsx
@@ -0,0 +1,4 @@
+import ArrowButtonLeft from '@/images/btn/btn_black_arrow_left.svg';
+import ArrowButtonRight from '@/images/btn/btn_black_arrow_right.svg';
+
+export { ArrowButtonLeft, ArrowButtonRight };
