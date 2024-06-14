512510305b47dbde05f3260767f39e5a5366663d
hong <hsw5971@naver.com>
2024-06-14 15:21:03 +0900

    fix: 네이밍 수정 onSelect -> onSelected

--- src/components/Category&Filter/Category/Category.tsx
+++ src/components/Category&Filter/Category/Category.tsx
@@ -7,9 +7,9 @@ const cn = classNames.bind(styles);
 interface CategoryProps {
   className?: string;
   list: string[];
-  onSelect: (index: number) => void;
+  onSelected: (index: number) => void;
 }
-export default function Category({ className, list, onSelect }: CategoryProps) {
+export default function Category({ className, list, onSelected }: CategoryProps) {
   const [clicked, setClicked] = useState<string | ''>('');
 
   const handleCategoryClick = (item: string) => {
@@ -24,9 +24,7 @@ export default function Category({ className, list, onSelect }: CategoryProps) {
             className={cn('wrapper', { clickedItem: clicked === item })}
             onClick={() => {
               handleCategoryClick(item);
-              if (onSelect) {
-                onSelect(index);
-              }
+              onSelected(index);
             }}
             key={`${index} ${item}`}
           >
