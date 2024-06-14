fac1f943d4b89e70fbd910aa66cda310227e511e
김동현 <dtc03003@naver.com>
2024-06-15 08:14:00 +0900

    refactor: 카드 컴포넌트 리팩토링

--- src/components/CardResource/CardContent.tsx
+++ src/components/CardResource/CardContent.tsx
@@ -1,21 +1,33 @@
 import classNames from 'classnames/bind';
-import styles from './CardContent.module.scss';
 import StarBold from '@/images/icon/icon_star_bold.svg';
-import { CardContentProps } from './CardResourceType';
+import styles from './CardContent.module.scss';
 
-export default function CardContent({ size, rating, reviewCount, price, children }: CardContentProps) {
+type cardSize = 'large' | 'small';
+
+interface CardContentProps {
+  activity: {
+    id: number;
+    title: string;
+    price: string;
+    rating: number;
+    reviewCount: number;
+  };
+  size: cardSize;
+}
+
+export default function CardContent({ activity, size = 'small' }: CardContentProps) {
   const cn = classNames.bind(styles);
 
   return (
     <div className={cn('cardContainer', size)}>
       <div className={cn('ratingContainer', size)}>
         <StarBold className={cn('starBold', size)} />
-        <div className={cn('rating', size)}>{rating}</div>
-        <div className={cn('reviewCount', size)}>({reviewCount})</div>
+        <div className={cn('rating', size)}>{activity.rating}</div>
+        <div className={cn('reviewCount', size)}>({activity.reviewCount})</div>
       </div>
-      <h1 className={cn('cardDescription', size)}>{children}</h1>
+      <h1 className={cn('cardDescription', size)}>{activity.title}</h1>
       <div className={cn('priceContainer', size)}>
-        ₩ {price}
+        ₩ {activity.price}
         <span className={cn('perPerson', size)}>/ 인</span>
       </div>
     </div>
--- src/components/CardResource/CardResource.tsx
+++ src/components/CardResource/CardResource.tsx
@@ -1,18 +1,44 @@
-import Image from 'next/image';
+import { useState } from 'react';
+import Image, { StaticImageData } from 'next/image';
 import classNames from 'classnames/bind';
-import styles from './CardResource.module.scss';
 import CardContent from './CardContent';
-import { CardResourceProps } from './CardResourceType';
+import errorImg from '@/images/img/img_resource_error.png';
+import styles from './CardResource.module.scss';
 
-export default function CardResource({ imgUrl, rating, reviewCount, price, children }: CardResourceProps) {
+interface CardResourceProps {
+  activity: {
+    id: number;
+    title: string;
+    price: string;
+    bannerImageUrl: string;
+    rating: number;
+    reviewCount: number;
+  };
+}
+
+export default function CardResource({ activity }: CardResourceProps) {
   const cn = classNames.bind(styles);
+  const [src, setSrc] = useState<string | StaticImageData>(activity.bannerImageUrl);
+
+  // 임시 데이터 (추후 수정 예정)
+  const BLUR_DATA_URL =
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP0rQcAAR8AzpZX2ywAAAAASUVORK5CYII=';
 
   return (
     <div className={cn('cardResource')}>
-      {imgUrl ? <Image className={cn('backgroundImg')} src={imgUrl} alt="배경화면" priority /> : null}
-      <CardContent size="large" rating={rating} reviewCount={reviewCount} price={price}>
-        {children}
-      </CardContent>
+      {activity.bannerImageUrl && (
+        <Image
+          className={cn('backgroundImg')}
+          src={src}
+          alt="배경화면"
+          width={700}
+          height={700}
+          onError={() => setSrc(errorImg)}
+          placeholder="blur"
+          blurDataURL={BLUR_DATA_URL}
+        />
+      )}
+      <CardContent size="large" activity={activity} />
     </div>
   );
 }
