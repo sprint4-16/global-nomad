a0aaaaae8e349b144c073e4102e75601897adae4
Youth-o <2021111495@dgu.ac.kr>
2024-06-16 00:22:55 +0900

    .

--- src/components/ActivityPost/ActivityPostForm/ActivityPostForm.tsx
+++ src/components/ActivityPost/ActivityPostForm/ActivityPostForm.tsx
@@ -112,7 +112,7 @@ export default function ActivityPostForm() {
   };
 
   const formatDate = (date: Date): string => {
-    const year = date.getFullYear().toString().slice(-2); // Get the last two digits of the year
+    const year = date.getFullYear().toString().slice(-2);
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const day = date.getDate().toString().padStart(2, '0');
     return `${year}/${month}/${day}`;
