a66ba0664cdf15bab85f902d761b758f01e1172f
hong <hsw5971@naver.com>
2024-06-17 17:27:43 +0900

    feat: 메인페이지 search 기능 구현 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import LandingLayout from '@/pageLayouts/LandingLayout/landingLayout';
 import ActivityListLayout from '@/pageLayouts/MainPageLayout/ActivityListLayout/ActivityListLayout';
+import SearchedListLayout from '@/pageLayouts/MainPageLayout/ActivityListLayout/SearchedListLayout';
+import { useState } from 'react';
 
 export default function Home() {
+  const [keyword, setKeyword] = useState<undefined | string>();
+
   return (
     <>
-      <LandingLayout />
-      <ActivityListLayout />
+      <LandingLayout searched={keyword} setSearched={setKeyword} />
+      {keyword ? <SearchedListLayout searched={keyword} /> : <ActivityListLayout />}
     </>
   );
 }
