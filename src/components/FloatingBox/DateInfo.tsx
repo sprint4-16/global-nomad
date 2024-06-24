b33748c58dcaf39882027347f4b89c800a173f7e
Taejin Kim <rlaxowls1316@naver.com>
2024-06-25 00:32:40 +0900

    resolve conflicts

--- src/components/FloatingBox/DateInfo.tsx
+++ src/components/FloatingBox/DateInfo.tsx
@@ -5,6 +5,7 @@ import CustomedDatePicker from '@/components/CustomedDatePicker/CustomedDatePick
 import styles from './FloatingBox.module.scss';
 import { useMediaQuery } from 'react-responsive';
 import { Time } from '@/types/activities.types';
+import ReservationDatePickPopover from '../Popover/ReservationDatePickPopover/ReservationDatePickPopover';
 
 const cn = classNames.bind(styles);
 
@@ -28,61 +29,72 @@ export default function DateInfo({
   const isTalbet = useMediaQuery({ query: '(max-width: 745px)' });
   const [tabletSize, setTabletSize] = useState(false);
 
-  const [selectedDate, setSelectedDate] = useState('날짜 선택하기');
-
-  const handleTempClick = () => {
-    setSelectedDate(datepick.toLocaleDateString() + ' 15:00 ~ 16:00');
-  };
+  const [isPopoverOpened, setIsPopoverOpened] = useState(false);
 
   useEffect(() => {
     setTabletSize(isTalbet);
   }, [isTalbet]);
 
   return (
-    <div className={cn('dateInfo')}>
-      {!tabletSize ? (
-        <div>
-          <div className={cn('mainText')}>날짜</div>
-          <CustomedDatePicker
-            selected={datepick}
-            onChange={(date) => {
-              onChangeDatepick(date);
-            }}
-            onMonthChange={(date) => {
-              onChangeDatepick(date);
-            }}
-            availableDates={availableDates}
-          />
-        </div>
-      ) : (
-        <>
-          <div className={cn('mainText')}>날짜</div>
-          <div className={cn('selectDate')} onClick={handleTempClick}>
-            {selectedDate}
-          </div>
-        </>
+    <>
+      {isPopoverOpened && (
+        <ReservationDatePickPopover
+          onMonthChange={(date) => {
+            onChangeDatepick(date);
+          }}
+          availableDates={availableDates}
+          onClose={() => setIsPopoverOpened(false)}
+          obj_mapped_date_times={obj_mapped_date_times}
+          onChangeScheduleId={onChangeScheduleId}
+          datepick={datepick}
+          scheduleId={scheduleId}
+        />
       )}
-      <div className={cn('reservationTime')}>
-        <div className={cn('mainText')}>예약 가능한 시간</div>
-        <div className={cn('timeBtns')}>
-          {obj_mapped_date_times &&
-            `${datepick.getFullYear()}-${(datepick.getMonth() + 1).toString().padStart(2, '0')}-${datepick.getDate().toString().padStart(2, '0')}` in
-              obj_mapped_date_times &&
-            obj_mapped_date_times[
-              `${datepick.getFullYear()}-${(datepick.getMonth() + 1).toString().padStart(2, '0')}-${datepick.getDate().toString().padStart(2, '0')}`
-            ]?.map((time) => (
-              <div
-                key={time.id}
-                className={cn('btn', { btnClicked: time.id === scheduleId })}
-                onClick={() => onChangeScheduleId(time.id)}
-              >
-                {time.startTime}~{time.endTime}
-              </div>
-            ))}
-          {/* <div className={cn('btnClicked')}>15:00~16:00</div> */}
+      <div className={cn('dateInfo')}>
+        {!tabletSize ? (
+          <div>
+            <div className={cn('mainText')}>날짜</div>
+            <CustomedDatePicker
+              selected={datepick}
+              onChange={(date) => {
+                onChangeDatepick(date);
+              }}
+              onMonthChange={(date) => {
+                onChangeDatepick(date);
+              }}
+              availableDates={availableDates}
+            />
+          </div>
+        ) : (
+          <>
+            <div className={cn('mainText')}>날짜</div>
+            <div className={cn('selectDate')} onClick={() => setIsPopoverOpened(true)}>
+              날짜 선택하기
+            </div>
+          </>
+        )}
+        <div className={cn('reservationTime')}>
+          <div className={cn('mainText')}>예약 가능한 시간</div>
+          <div className={cn('timeBtns')}>
+            {obj_mapped_date_times &&
+              `${datepick.getFullYear()}-${(datepick.getMonth() + 1).toString().padStart(2, '0')}-${datepick.getDate().toString().padStart(2, '0')}` in
+                obj_mapped_date_times &&
+              obj_mapped_date_times[
+                `${datepick.getFullYear()}-${(datepick.getMonth() + 1).toString().padStart(2, '0')}-${datepick.getDate().toString().padStart(2, '0')}`
+              ]?.map((time) => (
+                <div
+                  key={time.id}
+                  className={cn('btn', { btnClicked: time.id === scheduleId })}
+                  onClick={() => onChangeScheduleId(time.id)}
+                >
+                  {time.startTime}~{time.endTime}
+                </div>
+              ))}
+            {/* <div className={cn('btnClicked')}>15:00~16:00</div> */}
+          </div>
         </div>
+        <Stroke width="100%" className={cn('stroke')} />
       </div>
-      <Stroke width="100%" className={cn('stroke')} />
-    </div>
+    </>
   );
 }
--- src/components/Popover/ReservationDatePickPopover/ReservationDatePickPopover.tsx
+++ src/components/Popover/ReservationDatePickPopover/ReservationDatePickPopover.tsx
@@ -4,16 +4,33 @@ import styles from './ReservationDatePickPopover.module.scss';
 import CustomedDatePicker from '../../CustomedDatePicker/CustomedDatePicker';
 import { CSSProperties } from 'react';
 import Button from '@/components/Button/Button';
+import { Time } from '@/types/activities.types';
 
 interface ReservationDatePickPopoverProps {
   onClose: () => void;
   className?: string;
   sx?: CSSProperties;
+  onMonthChange: (date: Date) => void;
+  availableDates: string[];
+  obj_mapped_date_times: { [key: string]: Time[] };
+  onChangeScheduleId: (scheduleId: number) => void;
+  datepick: Date;
+  scheduleId: number;
 }
 
 const cn = classNames.bind(styles);
 
-export default function ReservationDatePickPopover({ sx, className, onClose }: ReservationDatePickPopoverProps) {
+export default function ReservationDatePickPopover({
+  sx,
+  className,
+  onClose,
+  onMonthChange,
+  availableDates,
+  obj_mapped_date_times,
+  onChangeScheduleId,
+  datepick,
+  scheduleId,
+}: ReservationDatePickPopoverProps) {
   return (
     <div style={sx} className={cn('container', className)}>
       <Header title="날짜" onClose={onClose} />
@@ -23,6 +40,8 @@ export default function ReservationDatePickPopover({ sx, className, onClose }: R
           onChange={(date: Date) => {
             console.log(date);
           }}
+          onMonthChange={onMonthChange}
+          availableDates={availableDates}
         />
       </div>
       <section>
@@ -31,9 +50,23 @@ export default function ReservationDatePickPopover({ sx, className, onClose }: R
           <li className={cn('tagListItem')}>
             <span className={cn('tag', 'active')}>14:00~15:00</span>
           </li>
-          <li className={cn('tagListItem')}>
-            <span className={cn('tag')}>15:00~16:00</span>
-          </li>
+          <div className={cn('timeBtns')}>
+            {obj_mapped_date_times &&
+              `${datepick.getFullYear()}-${(datepick.getMonth() + 1).toString().padStart(2, '0')}-${datepick.getDate().toString().padStart(2, '0')}` in
+                obj_mapped_date_times &&
+              obj_mapped_date_times[
+                `${datepick.getFullYear()}-${(datepick.getMonth() + 1).toString().padStart(2, '0')}-${datepick.getDate().toString().padStart(2, '0')}`
+              ]?.map((time) => (
+                <div
+                  key={time.id}
+                  className={cn('btn', { btnClicked: time.id === scheduleId })}
+                  onClick={() => onChangeScheduleId(time.id)}
+                >
+                  {time.startTime}~{time.endTime}
+                </div>
+              ))}
+            {/* <div className={cn('btnClicked')}>15:00~16:00</div> */}
+          </div>
         </ul>
       </section>
 
