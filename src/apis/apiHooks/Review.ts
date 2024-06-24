cfd3ae971e7f876f83b33fa6f813e745df06c413
Youth-o <2021111495@dgu.ac.kr>
2024-06-24 23:14:48 +0900

    fix: activityId로 리뷰 불러오도록 수정

--- src/apis/apiHooks/Review.ts
+++ src/apis/apiHooks/Review.ts
@@ -5,7 +5,7 @@ import { Reviews } from '@/types/review.types';
 
 export function useGetReviews({ activityId, page, size }: { activityId: string; page: number; size: number }) {
   return useQuery<Reviews>({
-    queryKey: ['review'],
+    queryKey: ['review', activityId],
     queryFn: async () => {
       const { data } = await axiosInstance.get(
         `${END_POINT.ACTIVITIES}/${activityId}/reviews?page=${page}&size=${size}`,
